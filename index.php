2d50764f15acc4a2dd750e9ba35227c08af2ce16
Agung Hari Wijaya <a9un9_ch@yahoo.co.id>
2014-03-15 11:23:06 +0700

    show title,date,author,category,content

--- index.php
+++ index.php
@@ -6,9 +6,6 @@ use ThauEx\SimpleHtmlDom\SHD;
 // get post url
 function crawler($page = 0)
 {	
-	if($page == 2)
-		die();
-
 	$url = 'http://rumaysho.com/arsip-artikel';
 	$apiUrl = 'http://localhost/dop/rumaysho-api/';
 	$currentUrl = ( $page != 0 ) ? $url."//page//".$page : $url;
@@ -16,7 +13,7 @@ function crawler($page = 0)
 	$html = SHD::fileGetHtml($currentUrl);
 	foreach($html->find('div.list_item') as $element) 
 		foreach ($element->find('h3 a') as $key)
-	       echo '<li>'.$key->href.'</li>';
+	       getPost($key->href);
 
 	$paging = $html->find('div.pagination',0);
 	$lastpaging = $paging->lastChild()->plaintext;
@@ -25,6 +22,38 @@ function crawler($page = 0)
 		crawler($page);
 
 }
+// get post data
+function getPost($url = '')
+{
+	SHD::$fileCacheDir = "tmp";
+	$html = SHD::fileGetHtml($url);
+	$post = $html->find('div.post',0);
+	$title = $post->find('h2.post-title',0);
+	$date = $post->find('p.post-meta span.date',0);
+	$author = $post->find('p.post-meta span.author',0);
+	$cat = $post->find('p.post-meta span.meta-cat a',0);
+	$content = $post->find('div.post_content',0);
+	echo '<li>';
+	echo '<p>Title : '.$title->plaintext.'</p>';
+	echo '<p>Date : '.$date->plaintext.'</p>';
+	echo '<p>Author : '.$author->plaintext.'</p>';
+	echo '<p>Category : '.$cat->plaintext.'</p>';
+	echo '<p>Content :</p>';
+	echo $content->innertext;
+	echo '</li>';
+}
+echo '
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+';
 echo "<ol>";
 crawler();
 echo "</ol>";
+echo "
+</body>
+</html>";
